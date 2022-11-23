32eb4c62931edd435827e22192f7a42abff79869
matiasvlevi <matiasvlevi@gmail.com>
2022-11-22 23:40:06 -0500

    multiple changes (see desc)
    DB is now used as an instance, and doesnt read/write from files while
    reading messages but needs to be load/saved at apropriate times.
    
    `getMessages` return heap stored messages
    `save` Saves the heap values to a file
    `load` Loads the file json to heap
    `loadIfExists` load the file, only if it exists, if it doesnt, return a BOT message

--- index.js
+++ index.js
@@ -2,43 +2,58 @@ const fs = require('node:fs');
 
 // Definition de DB 
 class DB {
- 
+
+  /**
+  * Load the local server file if it exists
+  */ 
+  loadIfExists(channel) {
+    if (!fs.existsSync(`./data/${channel}.json`)) {
+      let d = new Date();
+      return {
+        messages:[{
+          username:'Marvin',
+          message:`\x1b[90mThis chat room \x1b[32m${channel}\x1b[90m was created at ${d.toTimeString().split(' ')[0]} on ${d.toDateString()}\x1b[0m`,
+          userTag:'BOT'
+        }]
+      }; 
+    }
+    return JSON.parse(fs.readFileSync(`./data/${channel}.json`, 'utf-8'));
+  }
+
+  /**
+  * Load the local server file
+  */ 
+  load(channel) {
+    const data = this.loadIfExists(channel);   
+    this.messages[channel] = data.messages;
+  } 
   
   /**
    * get all messages in DB
    * @returns obj message list
    */
-  static getMessages(channel) {
-    if(!fs.existsSync('./data')){
-      fs.mkdirSync('./data');
-    }
-
-    if(fs.existsSync('./data/' + channel + '.json')){
-      let words = JSON.parse(fs.readFileSync('./data/' + channel + '.json','utf-8'));
-      return words;
-    }
-    else{
-      
-      fs.writeFileSync('./data/' + channel + '.json','[]');
-      return [];
-    }
+  getMessages(channel) {
+   return this.messages[channel]; 
+  }
+  
+  /**
+  * Save to a local server file
+  */ 
+  save(channel) {
+    fs.writeFileSync(`./data/${channel}.json`, JSON.stringify({
+      messages: this.messages[channel]
+    }), 'utf-8');
   }
 
   /**
    * add a message to DB
    * @param {Message} msg - the message to add to the DB 
    */
-  static addMessage(channel, msg) {
-
-    // Not existing directory guard
-    if (!fs.existsSync('./data'))
-     fs.mkdirSync('./data');
-   
+  addMessage(channel, msg) {   
     // Add a message
-    let msgs = DB.getMessages(channel);
-    msgs.push(msg);
+    this.messages[channel].push(msg);
+
     let str_msgs = JSON.stringify(msgs);
-    fs.writeFileSync('./data/' + channel + '.json',str_msgs);
   }
 
 }
