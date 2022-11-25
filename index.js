ee6804ce537597303254273f24d4f807d301c426
matiasvlevi <matiasvlevi@gmail.com>
2022-11-25 11:25:24 -0500

    changes, unstable

--- index.js
+++ index.js
@@ -2,28 +2,48 @@ const fs = require('node:fs');
 
 // Definition de DB 
 class DB {
+  constructor() {
+    this.messages = {};
+  }
+  static DEFAULT =(
+    channel,
+    time = new Date()
+  ) => {
+
+    let time_of_day = time.toTimeString().split(' ')[0];
+    let date = time.toDateString();
+    return {
+      messages:[{
+        username:'Marvin',
+        message:`\x1b[90mThis chat room \x1b[32m${channel}\x1b[90m was created at ${time_of_day} on ${date}\x1b[0m`,
+        userTag:'BOT'
+      }]
+    }
+
+  }
 
   /**
   * Load the local server file if it exists
   */ 
   loadIfExists(channel) {
-    if (!fs.existsSync(`./data/${channel}.json`)) {
-      let d = new Date();
-      return {
-        messages:[{
-          username:'Marvin',
-          message:`\x1b[90mThis chat room \x1b[32m${channel}\x1b[90m was created at ${d.toTimeString().split(' ')[0]} on ${d.toDateString()}\x1b[0m`,
-          userTag:'BOT'
-        }]
-      }; 
+    console.log(channel, this.messages);
+    if (
+      !fs.existsSync(`./data/${channel}.json`)
+    ) {
+      // default bot greeting for new channels
+      return DB.DEFAULT(channel);
     }
-    return JSON.parse(fs.readFileSync(`./data/${channel}.json`, 'utf-8'));
+
+    // read from file
+    return JSON.parse(
+      fs.readFileSync(`./data/${channel}.json`, 'utf-8')
+    );
   }
 
   /**
   * Load the local server file
   */ 
-  load(channel) {
+  load(channel) { 
     const data = this.loadIfExists(channel);   
     this.messages[channel] = data.messages;
   } 
@@ -33,7 +53,7 @@ class DB {
    * @returns obj message list
    */
   getMessages(channel) {
-   return this.messages[channel]; 
+   return [...this.messages[channel]]; 
   }
   
   /**
@@ -49,15 +69,17 @@ class DB {
    * add a message to DB
    * @param {Message} msg - the message to add to the DB 
    */
-  addMessage(channel, msg) {   
+  addMessage(channel, msg) {
+
+    if (this.messages[channel] === undefined) {
+      console.log('DEBUG: ', this.messages);
+      return;
+    }
+
     // Add a message
     this.messages[channel].push(msg);
-
-    let str_msgs = JSON.stringify(msgs);
   }
 
 }
 
 module.exports = DB;
-
-
